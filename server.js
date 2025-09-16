faffef0c5c87c45c6117ecdcb9f2fc891e484b70
tjwkdgus1 <seojanghyeon1@gmail.com>
2025-09-16 17:53:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 const server = http.createServer(app);
 const io = new Server(server, {
